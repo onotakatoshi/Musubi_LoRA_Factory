5ef78e4cdb99c15899cd25b1cf76468fcca36150
onotakatoshi <ono.takatoshi@gmail.com>
2026-07-05 07:01:06 +0900

    Apply orange help mark patch

--- app/desktop_launcher.py
+++ app/desktop_launcher.py
@@ -6,7 +6,8 @@ from pathlib import Path
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
-from desktop_main import DesktopApp
+from desktop_main import DesktopApp, HelpLabel
+from help_label_patch import apply_help_label_patch
 from language_patch import apply_language_patch as apply_ui_language_patch
 from training_engine import TrainingEngine
 from training_engine_patch import apply_training_engine_patch
@@ -17,6 +18,7 @@ from ui_font import apply_balanced_ui_font
 ROOT = Path(__file__).resolve().parents[1]
 ICON_PATH = ROOT / "assets" / "icons" / "musubi_lora_factory.svg"
 
+apply_help_label_patch(HelpLabel)
 apply_training_engine_patch(TrainingEngine)
 apply_training_tab_patch(DesktopApp)
 apply_ui_language_patch(DesktopApp)
